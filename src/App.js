679db7242a7d97ef4f7efc71a371c795fb764f34
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-02 17:43:54 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import './App.css';
+import { Configuration, OpenAIApi } from 'openai';
+
+const openai = new OpenAIApi(new Configuration({
+  apiKey: process.env.REACT_APP_API_KEY
+}));
+
+openai.createChatCompletion({
+  model: 'gpt-3.5-turbo',
+  messages: [{ role: 'user', content: 'Bozo moment' }]
+}).then(res => {
+  console.log(res.data.choices[0].message.content)
+})
 
 function App() {
   return (
