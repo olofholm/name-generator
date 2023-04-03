f62478f09b8edb8984db9ee937d1c03001a14f56
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-03 19:59:09 -0400

    UI Stuff

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { useState } from "react";
-import { TextField, Typography, Container, Button } from "@mui/material";
+import { TextField, Typography, Container, Button, ThemeProvider } from "@mui/material";
+import Header from "./components/header";
+import theme from "./theme";
 
 export default function Home() {
   const [descriptionInput, setDescriptionInput] = useState("");
@@ -34,16 +36,18 @@ export default function Home() {
   }
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Name my character</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
       </Head>
 
+      <Header/>
+    
       <Container maxWidth="sm" sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <Typography variant="h3" mb={3} mt={10}>Name My Character</Typography>
+        <Typography variant="h4" mb={3} mt={10}>Name My Character</Typography>
         <form onSubmit={onSubmit} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <TextField id="outlined-basic" label="Description" variant="outlined" sx={{ width: "400px" }}
+        <TextField id="outlined-basic" label="Description" variant="outlined" sx={{ width: "400px"}}
             type="text"
             name="description"
             placeholder="Character Description (e.g. 'A powerful warlock')"
@@ -55,6 +59,6 @@ export default function Home() {
         </form>
         <Typography mt={2}>{result}</Typography>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
