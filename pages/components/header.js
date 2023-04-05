527f20fc980c7a27577767b7837e6567948ae1f3
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-05 16:53:37 -0400

    Login and Logout with Auth0

--- pages/components/header.js
+++ pages/components/header.js
@@ -3,15 +3,17 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-export default function Header () {
+export default function Header(props) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Name Generator
+            Name & Image Generator
           </Typography>
+          {props.user ? (<Button color="inherit" href="/api/auth/logout">Logout</Button>) : (<Button color="inherit" href="/api/auth/login">Login</Button>)}
         </Toolbar>
       </AppBar>
     </Box>
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
   const [loadingImage, setLoadingImage] = useState(false);
   
   const { user, error, isloading} = useUser();
-  console.log(user);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -82,12 +81,14 @@ export default function Home() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
       </Head>
 
-      <Header/>
+      <Header user={user}/>
 
       <Container maxWidth="sm" sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
         <Typography mt={1} sx={{textAlign: "center"}}>Welcome to this name and image generator powered by AI.<br/>
         Enter a brief description and it will generate 3 names and one image based on your description. </Typography>
+
         <Typography variant="h5" mb={1} mt={3} sx={{fontWeight: "bold", textAlign: "center"}}>Start now by typing a description!</Typography>
+
         <form onSubmit={onSubmit} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
         <TextField id="outlined-basic" label="Description" variant="outlined" sx={{ width: "min(75vw, 400px)"}}
             type="text"
@@ -99,10 +100,12 @@ export default function Home() {
           <br/>
           <Button variant="contained" type="submit" value="Generate Names">Create</Button>
         </form>
+
         <Typography variant="h5" mt={2}><span style={{backgroundColor: "lightgrey"}}>{result1}</span></Typography>
         <Typography variant="h5"><span style={{backgroundColor: "lightgrey"}}>{result2}</span></Typography>
         <Typography variant="h5" mb={1}><span style={{backgroundColor: "lightgrey"}}>{result3}</span></Typography>
-          {loadingImage ? (<CircularProgress />) : (<Paper elevation={8} sx={{height: "min(95vw, 512px)", width: "min(95vw, 512px)"}}><img src={imageUrl} style={{borderRadius: "5px", width: "min(95vw, 512px)", height: "min(95vw, 512px)"}}></img></Paper>)}
+
+        {loadingImage ? (<CircularProgress />) : (<Paper elevation={8} sx={{height: "min(95vw, 512px)", width: "min(95vw, 512px)"}}><img src={imageUrl} style={{borderRadius: "5px", width: "min(95vw, 512px)", height: "min(95vw, 512px)"}}></img></Paper>)}
       </Container>
     </>
   );
