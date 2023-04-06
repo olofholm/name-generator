c587e0a12b4a6c30ca0275557a9a1bbe8c93a381
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-06 06:14:17 -0400

    Typo

--- pages/components/imageNotLoggedIn.js
+++ pages/components/imageNotLoggedIn.js
@@ -4,7 +4,7 @@ export default function ImageNotLoggedIn(props) {
   return (
   <>
     <Paper elevation={8} sx={{height: "min(95vw, 512px)", width: "min(95vw, 512px)"}}>
-      <Typography variant="h5" sx={{textAlign: "center"}} mt={3}>Please log in to generate a image</Typography>
+      <Typography variant="h5" sx={{textAlign: "center"}} mt={3}>Please log in to generate an image</Typography>
     </Paper>  
   </>
   );
