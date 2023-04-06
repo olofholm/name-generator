e756de5ebad3de88108e386a4b37e3d86fdc0c58
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-06 07:16:51 -0400

    Fixed icon

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,9 @@ export default function MyApp({ Component, pageProps }) {
       <ThemeProvider theme={theme}>
         <Head>
           <title>Name Generator</title>
-          <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
           <meta
           name="description"
