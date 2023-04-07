1bb5e71c44762c2e0d52580e4da39bae99a6af7e
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-07 06:58:47 -0400

    Next link instead of a

--- components/imageNotLoggedIn.js
+++ components/imageNotLoggedIn.js
@@ -1,10 +1,11 @@
 import { Paper, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function ImageNotLoggedIn(props) {
   return (
   <>
     <Paper elevation={8} sx={{height: "min(95vw, 512px)", width: "min(95vw, 512px)"}}>
-      <Typography variant="h5" sx={{textAlign: "center"}} m={3}>Please <span style={{backgroundColor: "lightgrey"}}><a href="/api/auth/login">log in</a></span> and verify email to start generating images</Typography>
+      <Typography variant="h5" sx={{textAlign: "center"}} m={3}>Please <span style={{backgroundColor: "lightgrey"}}><Link href="/api/auth/login">log in</Link></span> and verify email to start generating images</Typography>
     </Paper>  
   </>
   );
