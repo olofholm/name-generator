c9ee906358d949c5b0bbdaef5dc7db8488f10139
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-07 06:22:17 -0400

    Fixed footer jumping up on load

--- pages/components/generatedImage.js
+++ pages/components/generatedImage.js
@@ -1,10 +1,10 @@
-import { CircularProgress, Paper } from "@mui/material";
+import { CircularProgress, Paper, Box } from "@mui/material";
 
 export default function GeneratedImage(props) {
   return (
   <>
     {props.loadingImage ?
-      (<CircularProgress />) : 
+      (<Box sx={{height: 512}}><CircularProgress /></Box>) : 
         (<Paper elevation={8} sx={{height: "min(95vw, 512px)", width: "min(95vw, 512px)"}}>
           <img src={props.imageUrl} style={{borderRadius: "5px", width: "min(95vw, 512px)", height: "min(95vw, 512px)"}}>
           </img>
