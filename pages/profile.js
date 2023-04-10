91a88fd7bce09a493e573307d2a61feccd7ee58c
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-10 07:12:35 -0400

    Tokens

--- pages/profile.js
+++ pages/profile.js
@@ -6,11 +6,6 @@ import { useState, useEffect } from "react";
 export default function Profile () {
 
     const { user, error, isloading} = useUser();
-    const {tokens, setTokens} = useState(0);
-
-    const seeUser = () => {
-      console.log(user);
-    }
 
     //If the user is not logged in
     if(!user) {
@@ -18,7 +13,6 @@ export default function Profile () {
         <>
           <Header user={user} title="Profile" />
           <Typography variant="h4" sx={{textAlign: "center"}} mt={5} >Please Log in to view profile</Typography>
-          <Typography>{tokens}</Typography>
         </>
       );
     }
@@ -29,7 +23,6 @@ export default function Profile () {
       <>
         <Header user={user} title="Profile" />
         <Typography variant="h4" sx={{textAlign: "center"}} mt={5} >Welcome {user.nickname}!</Typography>
-        <Button onClick={seeUser}>Click</Button>
       </>
       );
     }
