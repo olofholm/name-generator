548f94d4fb648268950fae625c83d602b3db3ba7
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-12 05:48:26 -0400

    Decrease tokens function added

--- functions/index.js
+++ functions/index.js
@@ -12,3 +12,37 @@ exports.createUserDocument = functions.auth.user().onCreate((user) => {
       email: user.email    
     });
 });
+
+exports.decreaseTokens = functions.https.onRequest((req, res) => {
+  const documentId = req.query.documentId; // get the document ID from the request query params
+
+  if (!documentId) {
+    return res.status(400).send('Missing required parameter: documentId');
+  }
+
+  const docRef = admin.firestore().collection('users').doc(documentId);
+
+  docRef.get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).send('Document not found');
+      }
+
+      const data = doc.data();
+      const tokens = data.tokens;
+
+      if (!tokens || tokens <= 0) {
+        return res.status(400).send('Tokens value must be above 0');
+      }
+
+      // Decrease the tokens value in the document
+      return docRef.update({ tokens: admin.firestore.FieldValue.increment(-1) });
+    })
+    .then(() => {
+      res.send('Tokens decreased');
+    })
+    .catch((error) => {
+      console.error('Error decreasing tokens:', error);
+      res.status(500).send('Error decreasing tokens');
+    });
+});
