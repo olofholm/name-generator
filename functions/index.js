0c1e92bfed4ff0654f6ed9cc2a20b1be09f039d1
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-11 20:31:09 -0400

    Fixed tokens

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,10 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { addDoc } = require("firebase/firestore");
 admin.initializeApp();
 const db = admin.firestore();
 
-exports.createUserDocument = functions.auth.user().onCreate(() => {
-    addDoc(collection(db, "users"), {
-        uid: user.uid,
-        tokens: 5
-    });
+exports.createUserDocument = functions.auth.user().onCreate((user) => {
+  db.collection("users")
+    .doc(user.uid)
+    .set({ tokens: 5 });
 });
