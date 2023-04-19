c1d88e9c266faa633065c1ebfaa8d9a6f6d15e0b
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-04-19 19:30:14 -0400

    Small fixes

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
     }
   });
 
-  //When user submits a create request for AI
+  //When user submits a create request for tehAI
   async function onSubmit(event) {
     event.preventDefault();
     setLoadingImage(true);
